d353fe4e97cdce87a576718f9e7ab6bab574245c
rafa <rlima.marques@outlook.pt>
2024-06-19 19:44:44 +0100

    Navigation works

--- app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -17,39 +18,31 @@ import androidx.compose.foundation.layout.requiredHeightIn
 import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Switch
-import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.graphics.colorspace.ColorSpaces
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.NavController
+import com.example.bmicalculator.ui.theme.BMIBeginningLayout
 import com.example.bmicalculator.ui.theme.BMICalculatorTheme
-import kotlin.math.pow
+import com.example.bmicalculator.ui.theme.BMICalculatorLayout
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -59,202 +52,15 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = MaterialTheme.colorScheme.background,
                 ) {
-                    BMILayout()
+                    val navController = rememberNavController()
+                    NavHost(navController, startDestination = "home") {
+                        composable("home") { BMIBeginningLayout(navController) }
+                        composable("calculator") { BMICalculatorLayout() }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun EditNumberField(
-    modifier: Modifier = Modifier,
-    labelText: String,
-    value: String,
-    onValueChange: (String) -> Unit,
-    action: ImeAction = ImeAction.Next,
-) {
-
-    TextField(
-        value = value,
-        onValueChange = onValueChange,
-        singleLine = true,
-        label = { Text (text = labelText) },
-        keyboardOptions = KeyboardOptions.Default.copy(
-            keyboardType = KeyboardType.Number,
-            imeAction = action
-        ),
-        modifier = modifier
-
-    )
-}
-
-private fun calculateBMI(metric: Boolean, iWeight: Double, iHeight: Double): Int {
-    var bmi = iWeight/ iHeight.pow(2.0)
-    if (!metric) {
-        bmi = (703*iWeight)/ iHeight.pow(2.0)
-    }
-    return bmi.toInt()
-}
-
-@Composable
-fun MetricSystemRow(metric: Boolean,
-                    onMetricChanged: (Boolean) -> Unit,
-                    modifier: Modifier = Modifier){
-    Row(modifier = modifier
-        .fillMaxWidth()
-        .size(60.dp),
-        horizontalArrangement = Arrangement.Center,
-        verticalAlignment = Alignment.CenterVertically){
-        Text(
-            modifier = modifier
-                .fillMaxWidth()
-                .wrapContentWidth(Alignment.Start),
-            text = stringResource(R.string.metric_system),
-            fontFamily = FontFamily.Monospace,
-            fontSize = 15.sp,
-            fontWeight = FontWeight.Light,
-        )
-        Switch(
-            modifier = modifier
-                .fillMaxWidth(),
-            checked = metric,
-            onCheckedChange = onMetricChanged,
-            colors = SwitchDefaults.colors(
-                checkedThumbColor = Color(red = 122, green = 72, blue = 122, alpha = 255),
-                checkedTrackColor = Color(red = 77, green = 51, blue = 124, alpha = 255),
-                checkedBorderColor = Color.Black,
-                uncheckedThumbColor = Color(red = 200, green = 109, blue = 200),
-                uncheckedTrackColor = Color(red = 77, green = 51, blue = 124),
-                uncheckedBorderColor = Color.Black,
-            )
-            //ButtonDefaults.buttonColors(containerColor = Color(red = 200, green = 109, blue = 200))
-        )
-    }
-}
-
-@Composable
-fun BMILayout() {
-
-    var metric by remember { mutableStateOf(true) }
-
-    var inputAge by remember { mutableStateOf("") }
-    //val iAge = inputAge.toIntOrNull() ?: 0
-
-    var inputWeight by remember { mutableStateOf("") }
-    val iWeight = inputWeight.toDoubleOrNull() ?: 0.0
-
-    var inputHeight by remember { mutableStateOf("") }
-    val iHeight = inputHeight.toDoubleOrNull() ?: 0.0
-
-    //val bmi = if(iHeight > 0.0 && iWeight > 0.0) calculateBMI(metric, iWeight, iHeight) else 0
-
-
-    /// AQUELE ROSA E PESSIMO
-    /// Falta mudar
-
-    Column(modifier = Modifier
-                .border(
-                    BorderStroke( width = 3.dp, color = Color.Black),
-                )
-    ) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .requiredHeightIn(min = 40.dp, max = 55.dp)
-                .background(
-                    color = Color(red = 200, green = 109, blue = 200),
-                )
-                .border(
-                    BorderStroke(width = 3.dp, color = Color.Black),
-                )
-        ) {
-            Text(
-                text = stringResource(R.string.bmi_calculator),
-                fontFamily = FontFamily.Monospace,
-                fontSize = 25.sp,
-                fontWeight = FontWeight.Bold,
-                modifier = Modifier
-                    .padding(start = 10.dp, top = 10.dp, bottom = 10.dp)
-            )
-        }
-        Column(
-            modifier = Modifier
-                .statusBarsPadding()
-                .padding(horizontal = 40.dp, vertical = 60.dp)
-                .verticalScroll(rememberScrollState()) //Important
-                .safeDrawingPadding(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center,
-        ) {
-
-            EditNumberField(
-                labelText = stringResource(R.string.age_in_years),
-                value = inputAge,
-                onValueChange = { inputAge = it },
-                modifier = Modifier
-                    .padding(top = 32.dp)
-                    .padding(bottom = 32.dp)
-                    .fillMaxWidth()
-            )
-            EditNumberField(
-                labelText = stringResource(R.string.weight_kg_lbs),
-                value = inputWeight,
-                onValueChange = { inputWeight = it },
-                modifier = Modifier
-                    .padding(bottom = 32.dp)
-                    .fillMaxWidth()
-            )
-            EditNumberField(
-                labelText = stringResource(R.string.height_m_in),
-                value = inputHeight,
-                onValueChange = { inputHeight = it },
-                modifier = Modifier
-                    .padding(bottom = 32.dp)
-                    .fillMaxWidth(),
-            )
-
-            MetricSystemRow(
-                metric = metric,
-                onMetricChanged = { metric = it },
-                modifier = Modifier.padding(bottom = 32.dp)
-            )
-            Button(
-                onClick = { /*TODO*/ },
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = Color(
-                        red = 200,
-                        green = 109,
-                        blue = 200
-                    )
-                ),
-                border = BorderStroke(width = 3.dp, color = Color.Black)
-            ) {
-                Text(
-                    text = "Calculate now ",
-                    fontFamily = FontFamily.Monospace,
-                    fontSize = 15.sp,
-                    fontWeight = FontWeight.Light,
-                    modifier = Modifier
-                        .padding(start = 10.dp, top = 10.dp, bottom = 10.dp)
-
-                )
-                Image(
-                    painter = painterResource(id = R.drawable.arrow),
-                    contentDescription = "Arrow",
-                    Modifier.size(50.dp)
-                )
-            }
-        }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BMICalculatorTheme {
-        BMILayout()
     }
 }
---
+++ app/src/main/java/com/example/bmicalculator/ui/theme/BeginningScreen.kt
@@ -0,0 +1,115 @@
+package com.example.bmicalculator.ui.theme
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredHeightIn
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.example.bmicalculator.R
+
+
+@Composable
+fun BMIBeginningLayout(navController : NavController) {
+
+    Column(modifier = Modifier
+        .fillMaxHeight()
+        .border(
+            BorderStroke( width = 3.dp, color = Color.Black),
+        )
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .requiredHeightIn(min = 40.dp, max = 55.dp)
+                .background(
+                    color = Color(red = 200, green = 109, blue = 200),
+                )
+                .border(
+                    BorderStroke(width = 3.dp, color = Color.Black),
+                )
+        ) {
+            Text(
+                text = stringResource(R.string.bmi_calculator),
+                fontFamily = FontFamily.Monospace,
+                fontSize = 25.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .padding(start = 10.dp, top = 10.dp, bottom = 10.dp)
+            )
+        }
+        Column(
+            modifier = Modifier
+                .statusBarsPadding()
+                .padding(horizontal = 40.dp, vertical = 60.dp)
+                .verticalScroll(rememberScrollState()) //Important
+                .safeDrawingPadding(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+        ) {
+
+
+            Button(
+                onClick = {
+                    navController.navigate("calculator") },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(
+                        red = 200,
+                        green = 109,
+                        blue = 200
+                    )
+                ),
+                border = BorderStroke(width = 3.dp, color = Color.Black)
+            ) {
+                Text(
+                    text = stringResource(R.string.head_to_calculator),
+                    fontFamily = FontFamily.Monospace,
+                    fontSize = 15.sp,
+                    fontWeight = FontWeight.Light,
+                    modifier = Modifier
+                        .padding(start = 10.dp, top = 10.dp, bottom = 10.dp)
+
+                )
+                Image(
+                    painter = painterResource(id = R.drawable.arrow),
+                    contentDescription = "Arrow",
+                    Modifier.size(50.dp)
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomePreview() {
+    BMICalculatorTheme {
+        BMIBeginningLayout(rememberNavController())
+    }
+}
